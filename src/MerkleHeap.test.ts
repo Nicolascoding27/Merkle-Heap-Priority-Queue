411a409ba6544d43168cf4f6fc55883e92adff74
Nicolas Felipe Diaz <68035598+Nicolascoding27@users.noreply.github.com>
2023-03-04 14:50:02 -0500

    First test working

--- src/MerkleHeap.test.ts
+++ src/MerkleHeap.test.ts
@@ -17,52 +17,53 @@ describe('Merkle Heap', () => {
   afterAll(async () => {
     setTimeout(shutdown, 300000);
   });
+//Functions
 
   //Test for constructor function
   //If the heap was initialized succesfully so it is empty the root should correspont to the hash of the child nodes which are Field(0) and its hashes.
   //In the case of Heigh=3 the root  must be equal to the H(h(Field(0),h(Field(0))))
   it('Initializing an empty Merkle Heap',async () => {
-    await isReady;
     const merkleHeap = new MerkleHeap (3)
     let merkleRoot = merkleHeap.getRoot();
-
+    console.log(merkleRoot)
     let hashOfZeroes = Poseidon.hash([new Field(0), new Field(0)]);
     let hashOfHashOfZeroes = Poseidon.hash([hashOfZeroes, hashOfZeroes]);
 
     expect(hashOfHashOfZeroes).toEqual(merkleRoot);
   });
+  
 
   //Tests for insert functionß
   //We need to  test that the Heap property is there after n number of insertions.
   describe('Insert function test',  () => {
-    //   it('');
-    //For H=3
-    const merkleHeap = new MerkleHeap(3);
-    merkleHeap.insert(Field(10));
-    merkleHeap.insert(Field(38));
-    merkleHeap.insert(Field(17));
-    merkleHeap.insert(Field(62));
-    verifyHeapProperty(merkleHeap);
-    function verifyHeapProperty(merkleHeap: MerkleHeap) {
-     // await isReady;
-     let nodes = 2 ^ (merkleHeap.height - 1);
-     let currentFather = merkleHeap.getMerkleTreeLeaf(0n);
-     for (let i = 0; i < nodes && currentFather !== null; i++) {
-       let children = merkleHeap.getChildIndexesOfFather(BigInt(i));
-       if (children.left !== null) {
-         let childrenLeftValue = merkleHeap.getMerkleTreeLeaf(children.left);
-         expect(childrenLeftValue?.toBigInt()).toBeGreaterThanOrEqual(
-           currentFather.toBigInt()
-         );
-       }
-       if (children.right !== null) {
-         let childrenRightValue = merkleHeap.getMerkleTreeLeaf(children.right);
-         expect(childrenRightValue?.toBigInt()).toBeGreaterThanOrEqual(
-           currentFather.toBigInt()
-         );
-       }
-       currentFather = merkleHeap.getMerkleTreeLeaf(BigInt(i + 1));
-     }
-   }
+      it('Verifies Heap property after 4 insertions for Height=3', async() => {
+        //   //For H=3
+          const merkleHeap = new MerkleHeap(3);
+          merkleHeap.insert(Field(10));
+          merkleHeap.insert(Field(38));
+          merkleHeap.insert(Field(17));
+          merkleHeap.insert(Field(62));
+        //   verifyHeapProperty(merkleHeap);
+        //   function verifyHeapProperty(merkleHeap: MerkleHeap) {
+            let nodes = 2 ^ (merkleHeap.height - 1);
+            let currentFather = merkleHeap.getMerkleTreeLeaf(0n);
+            for (let i = 0; i < nodes && currentFather !== null; i++) {
+              let children = merkleHeap.getChildIndexesOfFather(BigInt(i));
+              if (children.left !== null) {
+                let childrenLeftValue = merkleHeap.getMerkleTreeLeaf(children.left);
+                expect(childrenLeftValue?.toBigInt()).toBeGreaterThanOrEqual(
+                  currentFather.toBigInt()
+                );
+              }
+              if (children.right !== null) {
+                let childrenRightValue = merkleHeap.getMerkleTreeLeaf(children.right);
+                expect(childrenRightValue?.toBigInt()).toBeGreaterThanOrEqual(
+                  currentFather.toBigInt()
+                );
+              }
+              currentFather = merkleHeap.getMerkleTreeLeaf(BigInt(i + 1));
+            }
+        //   }
+        })
   });
 });
