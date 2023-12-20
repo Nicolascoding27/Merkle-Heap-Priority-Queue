083cb329fe9491ace6b086c2042372ed86d66eb8
SantiR <santiriosolaya@gmail.com>
2023-12-20 00:58:13 -0500

    feat: add e2e integration tests
    - update snarky version
    - fix bugs related with smart contract execution
    - test setup
    - deployment setup
    
    TODO: create test to show use cases with demo test
    
    Signed-off-by: SantiR <santiriosolaya@gmail.com>

--- src/demo.ts
+++ src/demo.ts
@@ -9,33 +9,57 @@ import {
   State,
   isReady,
   AccountUpdate,
+  DeployArgs,
+  Permissions,
 } from 'snarkyjs';
 await isReady;
 
+let proofsEnabled = false;
+// we need the initiate tree root in order to tell the contract about our off-chain storage
+let initialCommitment: Field = Field(0);
+
 const height = 3;
 const heap = new MerkleHeap(height);
-let proofsEnabled = false;
+
 export class MerkleHeapDemo extends SmartContract {
+  // private heap!: MerkleHeap;
+
+  @state(Field) commitment = State<Field>();
   @state(Field) heapRoot = State<Field>();
-  @method initState(initialValue: Field) {
-    heap.insert(initialValue);
-    //Check if the min is equal to the initial value as it is just one
-    let firstRoot = heap.findMin();
-    this.heapRoot.set(firstRoot!);
-    console.log('FIRST ROOT', firstRoot?.toBigInt());
+
+  /**
+   * Deploy function
+   * @param args  verification key
+   */
+  deploy(args: DeployArgs) {
+    super.deploy(args);
+
+    const permissionToEdit = Permissions.proof();
+
+    this.account.permissions.set({
+      ...Permissions.default(),
+      editState: permissionToEdit,
+      setTokenSymbol: permissionToEdit,
+      send: permissionToEdit,
+      receive: permissionToEdit,
+    });
   }
 
-  @method insertValue(value: Field) {
-    heap.insert(value);
-    const newRoot = heap.findMin();
+  @method initState(initialRoot: Field) {
+    super.init();
+    //Check if the min is equal to the initial value as it is just one
+
+    this.heapRoot.set(initialRoot);
+    this.commitment.set(initialCommitment);
+    // console.log('FIRST ROOT', firstRoot?.toBigInt());
+  }
+
+  @method insertValue(newRoot: Field) {
     this.heapRoot.set(newRoot!);
   }
-  // @method deleteValue(valueToDelete:Field){
-  // }
-  @method deleteMin() {
-    return heap.deleteMin();
-  }
 }
+
+// ----- refactor this shit
 // ZK APP SETUP
 let zkappKey = PrivateKey.random();
 let zkappAddress = zkappKey.toPublicKey();
@@ -45,35 +69,56 @@ Mina.setActiveInstance(Local);
 
 //Test Accounts
 let bonsaiTestPk = Local.testAccounts[0].privateKey;
+let bonsaiTestPublicKey = Local.testAccounts[0].publicKey;
 // let bonsaiTestAccount = bonsaiTestPk.toPublicKey();
+
 async function localDeploy(
   zkAppInstance: MerkleHeapDemo,
   zkAppPrivatekey: PrivateKey,
   bonsaiTestPk: PrivateKey
 ) {
+  heap.insert(Field(1));
+  let firstRoot = heap.findMin() as Field;
   let initialTx = await Mina.transaction(bonsaiTestPk, () => {
     AccountUpdate.fundNewAccount(bonsaiTestPk);
     zkapp.deploy({ zkappKey: zkappKey });
-    zkapp.initState(Field(1));
+    zkapp.initState(firstRoot);
     console.log('Contract deployed succedfully');
   });
   await initialTx.prove();
   initialTx.sign([zkAppPrivatekey]);
   await initialTx.send();
+
+  // await MerkleHeapDemo.compile();
 }
 await localDeploy(zkapp, zkappKey, bonsaiTestPk);
-//   await initialTx.send();
+console.log('Local deploy complete  >>>>>');
+
+// await CompileContract();
 
 async function insertAnddeleteElement(value: Field) {
-  let tx = await Mina.transaction(bonsaiTestPk, () => {
-    // zkapp.inser
-    zkapp.insertValue(value);
-    //This should be Field(1)
-    let firstDeletion = zkapp.deleteMin();
-    console.log('First deletion => ', firstDeletion?.toBigInt());
-  });
+  heap.insert(value);
+  const newRoot = heap.findMin() as Field;
+  let tx = await Mina.transaction(
+    bonsaiTestPk,
+    // { sender: bonsaiTestPublicKey },
+    () => {
+      // Check: Do i need to compile the contract?
+      // zkapp.inser
+      zkapp.insertValue(newRoot);
+      // //This should be Field(1)
+      // let firstDeletion = zkapp.deleteMin();
+      // TODO: use Circuit.log to print the value
+      // console.log('First Deletion >>>', firstDeletion);
+      // console.log('First deletion => ', firstDeletion?.toBigInt());
+    }
+  );
+  heap.deleteMin();
   await tx.prove();
   tx.sign([zkappKey]);
   await tx.send();
 }
 await insertAnddeleteElement(Field(22));
+
+// TODO: follow the example and adapt to Merkle heap implementation
+// https://github.com/o1-labs/o1js/blob/main/src/examples/zkapps/merkle_tree/merkle_zkapp.ts#L156
---
+++ tests/integration-tests/smart-contract-demo.test.ts
@@ -0,0 +1,105 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import {
+  isReady,
+  shutdown,
+  Poseidon,
+  Field,
+  UInt64,
+  UInt32,
+  Mina,
+  PublicKey,
+  PrivateKey,
+  fetchAccount,
+  Signature,
+  AccountUpdate,
+} from 'snarkyjs';
+
+import { MerkleHeapDemo } from '../../src/demo';
+import { waitUntilNextBlock } from '../../src/network';
+
+await isReady;
+///Setup
+const zkAppTestContractAddress = process.env.MERKLE_HEAP_DEMO_CONTRACT || '';
+const isBerkeley = process.env.TEST_NETWORK === 'true';
+
+// variables
+let zkAppTest: MerkleHeapDemo;
+
+beforeAll(async () => {
+  zkAppTest = new MerkleHeapDemo(
+    PublicKey.fromBase58(zkAppTestContractAddress)
+  );
+});
+
+describe('Merkle Heap Smart Contract Demo', () => {
+  let Blockchain;
+  let zkAppTestPrivateKey: any; // private key
+  let zkAppTestPublicKey: any; // public key
+  let localBlockchain: any;
+
+  beforeAll(async () => {
+    // Mina Blockchain instance : local | berkeley
+    if (isBerkeley) {
+      Blockchain = Mina.Network({
+        mina: 'https://proxy.berkeley.minaexplorer.com/graphql',
+        archive: 'https://archive-node-api.p42.xyz/',
+      });
+      zkAppTestPrivateKey = PrivateKey.fromBase58(
+        'EKDxPsv3rnVvk8MVp7A5UNaL9pTVXnQkYdikuas3pHPHJyBCn4YC'
+      );
+      zkAppTestPublicKey = PublicKey.fromBase58(
+        'B62qn3vM657WqhbgCtuxuxLjL6fSEkSu1CTJqSQA7uhcR9gc3uEKT1Z'
+      );
+      console.log('On Berkley ready');
+      Mina.setActiveInstance(Blockchain);
+    } else {
+      localBlockchain = Mina.LocalBlockchain();
+      zkAppTestPrivateKey = localBlockchain.testAccounts[0].privateKey;
+      zkAppTestPublicKey = zkAppTestPrivateKey.toPublicKey();
+      Mina.setActiveInstance(localBlockchain);
+    }
+    // compile contracts
+    await MerkleHeapDemo.compile();
+    // set initial state
+
+    await zkAppTest.initState(Field(1));
+  });
+
+  /**
+   * Waits until next block on Berkeley by polling the network state
+   * or mocks the next block on local blockchain by increasing block length by 1.
+   */
+  async function waitForNextBlock() {
+    if (isBerkeley) {
+      // provide parameters to overwrite defaults for number of retries and polling interval
+      await waitUntilNextBlock();
+    } else {
+      localBlockchain.setBlockchainLength(
+        localBlockchain.getNetworkState().blockchainLength.add(1)
+      );
+    }
+  }
+
+  afterAll(() => {
+    setInterval(shutdown, 0);
+  });
+
+  test('should insert a new value', async () => {
+    const valueToInsert = Field(2);
+    const insertTx = await Mina.transaction(
+      { sender: zkAppTestPublicKey, fee: 200_000_000 },
+      () => {
+        zkAppTest.insertValue(valueToInsert);
+      }
+    );
+    await insertTx.prove();
+    await insertTx.sign([zkAppTestPrivateKey]).send();
+
+    console.log('Farm.deposit() successful', insertTx.toPretty());
+
+    const account = await fetchAccount({ publicKey: zkAppTestPublicKey });
+    const root = account.state.get(zkAppTest.heapRoot);
+    insertTx.transaction.accountUpdates.expect(root).toEqual(valueToInsert);
+  });
+});
